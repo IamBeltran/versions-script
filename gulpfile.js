7ebf90da9db5f9c845db5b900f2bf6c733053bd2
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2019-03-07 14:34:26 -0600

    Add config file for gulp

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+/* eslint-disable no-console */
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │  REQUIRE THIRDPARTY-MODULES DEPENDENCY.                                           │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+
+// Array of dir path
+const ignore = ['!node_modules/**'];
+
+const inspect = [
+  'src/**/*.js',
+];
+
+const all = [...ignore, ...inspect];
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │  DECLARATION OF TASKS FOR GULP.                                                   │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+gulp.task('lint', () =>
+  gulp
+    .src(all)
+    .pipe(
+      eslint({
+        rules: {
+          'import/no-dynamic-require': 0,
+          'no-param-reassign': ['error', { props: false }],
+        },
+      }),
+    )
+    .pipe(eslint.format())
+    .pipe(
+      eslint.result(result => {
+        //  Called for each ESLint result.
+        console.log(`ESLint result: ${result.filePath}`);
+        console.log(`   # Messages: ${result.messages.length}`);
+        console.log(`   # Warnings: ${result.warningCount}`);
+        console.log(`     # Errors: ${result.errorCount}`);
+        console.log('----------------------------------------------------');
+      }),
+    )
+    .pipe(
+      eslint.results(results => {
+        //  Called once for all ESLint results.
+        //  callback(error);
+        console.log(` Total Results: ${results.length}`);
+        console.log(`Total Warnings: ${results.warningCount}`);
+        console.log(`  Total Errors: ${results.errorCount}`);
+        console.log('----------------------------------------------------');
+      }),
+    )
+    .pipe(eslint.failAfterError()),
+);
